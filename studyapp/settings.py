87644eda937bf5d0525efe4b12e8d9d80c5756c9
geoafrikana <geoafrikana@gmail.com>
2023-07-09 10:59:51 +0100

    serving static without whitenoise

--- studyapp/settings.py
+++ studyapp/settings.py
@@ -39,7 +39,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'studyapp.urls'
